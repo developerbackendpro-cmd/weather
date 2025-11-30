4b7761f223e69cbd99df41b6d8bbf071f5fcd54f
developerbackendpro-cmd <developerbackendpro@gmail.com>
2025-11-30 22:43:21 +0500

    Update aiogram.py

--- python-lesson/aiogram.py
+++ python-lesson/aiogram.py
@@ -19,25 +19,25 @@ def get_weather(user_input):
     temp_fahrenheit = round(data['main']['temp'])
     temp_celsius = round((temp_fahrenheit - 32) * 5 / 9)
     weather_descriptions = {
-        'Clouds': 'Bulutli',
-        'Clear': 'Toza',
-        'Few clouds': 'Kam bulutli',
-        'Scattered clouds': 'Tarqalgan bulutlar',
-        'Broken clouds': 'Yoyilgan bulutlar',
-        'Overcast': 'Bulutli',
-        'Rain': 'Yomg\'ir',
-        'Drizzle': 'Yog\'ingarchilik',
-        'Thunderstorm': 'Momaqaldiroq',
-        'Snow': 'Qor',
-        'Mist': 'Tuman',
-        'Fog': 'Kuchli Tuman'
+        'Clouds': 'Облачно',
+        'Clear': 'Чисто',
+        'Few clouds': 'Небольшая облачность',
+        'Scattered clouds': 'Переменная облачность',
+        'Broken clouds': 'Разорванная облачность',
+        'Overcast': 'Пасмурно',
+        'Rain': 'Дождь',
+        'Drizzle': 'Морось',
+        'Thunderstorm': 'Гроза',
+        'Snow': 'Снег',
+        'Mist': 'Туман',
+        'Fog': 'Сильный туман'
     }
-    description = weather_descriptions.get(weather, "Bilinmaydigan ob-havo")
-    return f"Ob-havo {user_input}da {description}\nHarorat {user_input}da {temp_celsius}ºC"
+    description = weather_descriptions.get(weather, "Неопределённая погода")
+    return f"Погода в {user_input}: {description}\nТемпература в {user_input} {temp_celsius}ºC"
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Salom! Shaharning nomini yozing va men sizga ob-havo ma'lumotlarini taqdim etaman.")
+    await message.reply("Здравствуйте !\nНапишите название города, и я предоставлю вам информацию о погоде.")
 
 @dp.message_handler()
 async def handle_message(message: types.Message):
